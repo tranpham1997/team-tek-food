98b6674af59ad1a9003dfc79dad15c06538cfa7f
Demo User <demouser@MacBook-Pro.local>
2015-07-27 16:58:28 -0700

    separate yelp.py

--- wheel-of-food/main.py
+++ wheel-of-food/main.py
@@ -24,25 +24,12 @@ class MainHandler(webapp2.RequestHandler):
         location = self.request.get('Location')
         return location
         # Logic/process info - do a search with Yelp API
-        def search(term, location):
-        #Query the Search API by a search term and location.
-        #Args:
-            #term (str): The search term passed to the API.
-            #location (str): The search location passed to the API.
-        #Returns:
-            #dict: The JSON response from the request.
-            term = restaurants
-            url_params = {
-                'term': term.replace(' ', '+'),
-                'location': location.replace(' ', '+'),
-                'limit': search_limit
-            }
-            return yelp.request(api_host, search_path, url_params=url_params)
+        result = yelp.search('restaurants', location)
         # Send a response.
         self.redirect('/search')
 class SearchHandler(webapp2.RequestHandler):
     def get(self):
-        pass
+        self.response.write(result)
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/search', SearchHandler)
