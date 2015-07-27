1d918a31cd8f495b01f3160f2587259987e56f67
Demo User <demouser@MacBook-Pro.local>
2015-07-27 14:58:32 -0700

    adding templates to main.py

--- wheel-of-food/main.py
+++ wheel-of-food/main.py
@@ -1,34 +1,35 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
+import jinja2
 import webapp2
 from google.appengine.api import urlfetch
 import json
 # from googlemaps import geocoding, client
+
+env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         # mapsClient = client.Client(key = 'AIzaSyDIH9iVlHtpMY0BsBd3F3sn43Bmf4YV4mI')
-        self.response.write('Hello world!')
         json_content = urlfetch.fetch('https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=AIzaSyDIH9iVlHtpMY0BsBd3F3sn43Bmf4YV4mI').content
         results = json.loads(json_content)['results']
 
         # self.response.write(geocoding.reverse_geocode(mapsClient, (40.714224,-73.961452)))
-        self.response.write(results[0]['formatted_address']
-        )
+        self.response.write(results[0]['formatted_address'])
+        template = env.get_template('home.html')
+        self.response.write(template.render())
+    def post(self):
+        # Get information from request
+        location = self.request.get('Location')
+        # Logic/process info - do a search with Yelp API
+
+        # Send a response.
+        self.redirect('/search')
+class SearchHandler(webapp2.RequestHandler):
+    def get(self):
+        pass
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/search', SearchHandler)
 ], debug=True)
