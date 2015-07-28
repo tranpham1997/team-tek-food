5317edb6dd6a1810d341eefe2db786859b8a0fdf
Demo User <demouser@MacBook-Pro.local>
2015-07-28 10:02:56 -0700

    Tran's edits

--- wheel-of-food/main.py
+++ wheel-of-food/main.py
@@ -12,37 +12,35 @@ env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         # mapsClient = client.Client(key = 'AIzaSyDIH9iVlHtpMY0BsBd3F3sn43Bmf4YV4mI')
-        json_content = urlfetch.fetch('https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=AIzaSyDIH9iVlHtpMY0BsBd3F3sn43Bmf4YV4mI').content
-        results = json.loads(json_content)['results']
-
         # self.response.write(geocoding.reverse_geocode(mapsClient, (40.714224,-73.961452)))
-        self.response.write(results[0]['formatted_address'])
         template = env.get_template('home.html')
         self.response.write(template.render())
     def post(self):
         # Get information from request
         location = self.request.get('Location')
-        return location
         # Logic/process info - do a search with Yelp API
         result = yelp.search('restaurants', location)
         # Send a response.
         self.redirect('/search')
 class SearchHandler(webapp2.RequestHandler):
     def get(self):
-<<<<<<< HEAD
         pass
 
 class LocationHandler(webapp2.RequestHandler):
     def get(self):
+
+        json_content = urlfetch.fetch('https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=AIzaSyDIH9iVlHtpMY0BsBd3F3sn43Bmf4YV4mI').content
+        results = json.loads(json_content)['results']
+        address = results[0]['formatted address']
+        variables = {
+        'address' : address
+        }
         template = env.get_template('glhome.html')
-        self.response.write(template.render())
+        self.response.write(template.render(variables))
 
-    def post(self):
-        pass
-
-=======
-        self.response.write(result)
->>>>>>> 27aea70423d34c542a8eb2b8cbc6931898f9a013
+        def post(self):
+                # Get information from request
+            location = self.request.get('Location')
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/search', SearchHandler),
